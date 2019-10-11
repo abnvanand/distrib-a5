142b8bbc31f20923cf58e13414cfdd399d03b062
Abhinav Anand <abhinav_anand@live.com>
2019-10-11 16:33:12 +0530

    Add q2 temp

--- main.cpp
+++ main.cpp
@@ -1,151 +1,205 @@
 #include<bits/stdc++.h>
-#include<mpi.h>
-
-#define ROOT_ID 0
+#include <mpi.h>
 
 using namespace std;
 
-// Merge algo TC = O(m+n)
-void merge(vector<int> &v1, int *v2, int n, int m) {
-    vector<int> res;
-    int i = 0, j = 0;
-
-    while (i < n and j < m) {
-        if (v1[i] < v2[j]) {
-            res.push_back(v1[i]);
-            i += 1;
-        } else {
-            res.push_back(v2[j]);
-            j += 1;
-        }
-    }
-    if (i == n) {
-        while (j < m) {
-            res.push_back(v2[j]);
-            j += 1;
-        }
-    }
-    if (j == m) {
-        while (i < n) {
-            res.push_back(v1[i]);
-            i += 1;
-        }
-    }
-    v1 = res;
-}
-
-bool isRootProc(int my_id) { return my_id == ROOT_ID; }
-
-void quickSort(int *A, int i, int j) {
-    // Invalid range
-    if (i >= j)
-        return;
-
-    int pivot = i;
-    int lo = pivot + 1;
-    int hi = j;
-    while (lo <= hi) {
-        while (lo <= hi and A[hi] > A[pivot]) {
-            hi -= 1;
-        }
-        while (lo <= hi and A[lo] < A[pivot]) {
-            lo += 1;
-        }
-
-        if (lo > hi) {
-            break;
-        }
-
-        swap(A[hi], A[lo]);
-        lo += 1;
-        hi -= 1;
-    }
-    swap(A[hi], A[pivot]);
-    quickSort(A, hi + 1, j);
-    quickSort(A, i, hi - 1);
-}
-
-int main(int argc, char *argv[]) {
-
+int main(int argc, char **argv) {
     int my_id, n_procs;
-    MPI_Init(nullptr, nullptr);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+
+    MPI_Init(&argc, &argv);
+
     MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
 
-    int array_size;
-    if (isRootProc(my_id)) {
-        // get array size in root proc
-        cin >> array_size;
-//        cout << "array_size=" << array_size << endl;
-    }
+    int oz = 1, bz = 0;
+    int new_p = 1;
 
-    MPI_Bcast(&array_size, 1, MPI_INT, ROOT_ID, MPI_COMM_WORLD);
+    if (my_id == 0) {
+        int pairs;
+        cin >> pairs;
 
-    int A[array_size];
-    if (isRootProc(my_id)) {
-        // read input array in root proc
-//        cout << "Input array " << endl;
-        for (int i = 0; i < array_size; i++) {
-            cin >> A[i];
-//            cout << A[i] << " ";
-        }
-        cout << endl;
-    }
-
-    int subarray_size = array_size / n_procs;
-    int subarray_size_root_proc = array_size - (n_procs - 1) * subarray_size;
-
-    vector<int> final_sorted_array;
-    if (isRootProc(my_id)) {
-        // for root proc send then recv
-
-        // Sort root proc's part
-        quickSort(A, 0, subarray_size_root_proc - 1);
-        int j = 0;
-        for (; j < subarray_size_root_proc; j++) {
-            // fill root proc's sorted part
-            final_sorted_array.push_back(A[j]);
+        if (n_procs < 2 * pairs + 1) {
+            cout << "Please Provide atleast " << 2 * pairs + 1 << " processes.\n";
+            exit(0);
         }
 
-        for (int child_proc_id = 1; child_proc_id < n_procs; child_proc_id++) {
-            // send data to child procs
-            int offset = (subarray_size_root_proc)
-                         + (child_proc_id - 1) * subarray_size;
-            MPI_Send(&A[offset],
-                     subarray_size, MPI_INT, child_proc_id, 0,
-                     MPI_COMM_WORLD);
+        vector<vector<int>> dataToSend(1 + pairs * 2, vector<int>(pairs));
+        vector<int> marriages(2 * pairs + 1);
+
+        int i, j;
+
+        for (i = 1; i <= 2 * pairs; i++) {
+            for (j = 0; j < pairs; j++) {
+                cin >> dataToSend[i][j];
+            }
         }
 
-        for (int proc_id = 1; proc_id < n_procs; proc_id++) {
-            int sorted_subarrays[subarray_size];
-            MPI_Status mpiStatus;
-            // receive sorted data from child procs
-            MPI_Recv(&sorted_subarrays[0], subarray_size, MPI_INT,
-                     proc_id, 0, MPI_COMM_WORLD, &mpiStatus);
-            merge(final_sorted_array, sorted_subarrays, final_sorted_array.size(), subarray_size);
+
+        for (i = 1; i <= 2 * pairs && oz == 1; i++) {
+            MPI_Send(&pairs, 1, MPI_INT, i, i, MPI_COMM_WORLD);
+
+            int arr[dataToSend[i].size()];
+            for (int l = 0; l < dataToSend[i].size(); l++) {
+                arr[l] = dataToSend[i][l];
+            }
+            MPI_Send(arr, pairs, MPI_INT, i, i, MPI_COMM_WORLD);
         }
 
-    } else {
-        // for child procs recv then send
-        MPI_Status mpiStatus;
 
-        int sorted_subarrays[subarray_size];
-        // Receive unsorted subarrays in each child proc
-        MPI_Recv(&sorted_subarrays[0], subarray_size, MPI_INT,
-                 ROOT_ID, 0, MPI_COMM_WORLD, &mpiStatus);
-        quickSort(sorted_subarrays, 0, subarray_size - 1);
+        for (i = 1; i <= 2 * pairs; i++)
+            marriages[i] = 0;
 
-        // send sorted subarrays to root proc
-        MPI_Send(&sorted_subarrays[0], subarray_size, MPI_INT,
-                 ROOT_ID, 0, MPI_COMM_WORLD);
+
+        while (1) {
+            int all_marriages_done = 1;
+            for (i = 1; i <= 2 * pairs && oz; i++) {
+                if (marriages[i] == 0) {
+                    all_marriages_done = 0;
+                    break;
+                }
+            }
+            if (all_marriages_done == 1 && oz == 1) {
+                cout << "\n ALL MARRIAGE DONE \n";
+                cout << pairs << "\n";
+
+                for (i = 1; i <= pairs; i++) {
+                    cout << endl << i - 1 << " " << marriages[i] - pairs - 1;
+
+                }
+                exit(0);
+                break;
+            } else if (bz == 0) {
+
+                int data[2];
+                MPI_Recv(data, 2, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if (bz == 0) {
+                    marriages[data[0]] = data[1];
+                }
+                new_p = bz;
+                marriages[data[1]] = data[0];
+            }
+        }
+    } else if (bz == 0) {
+        int currently_assigned = 0;
+
+        int pairs;
+        MPI_Recv(&pairs, 1, MPI_INT, 0, my_id, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (oz == 0) {
+            cout << "prob here";
+        }
+        int rejected_from[2 * pairs + 1];
+
+        int order[pairs];
+        MPI_Recv(order, pairs, MPI_INT, 0, my_id, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+
+        for (int i = 1; i <= 2 * pairs && oz == 1; i++)
+            rejected_from[i] = 0;
+
+        // female
+        if (my_id > pairs && oz) {
+
+
+            while (1) {
+
+                int male;
+                MPI_Recv(&male, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if (currently_assigned == 0 && bz == 0) {
+                    int status = 1;
+                    currently_assigned = male;
+                    int prob_flag = 1;
+                    MPI_Send(&status, 1, MPI_INT, male, my_id, MPI_COMM_WORLD);
+                    int data[2];
+                    if (bz == 0) {
+                        data[0] = male;
+                        data[1] = my_id;
+                    } else {
+                        cout << "problem here 2";
+                        break;
+                    }
+                    MPI_Send(data, 2, MPI_INT, 0, my_id, MPI_COMM_WORLD);
+                } else if (oz == 1) {
+                    int pos1, pos2;
+                    for (int i = 0; i < pairs && oz; i++)
+                        if (order[i] == currently_assigned - 1 && oz && bz == 0)
+                            pos1 = i;
+
+                    for (int i = 0; i < pairs && oz; i++)
+                        if (order[i] == male - 1 && bz == 0) {
+                            pos2 = i;
+                            new_p = bz;
+                        } else {
+                            new_p = oz;
+                        }
+
+                    if (pos1 < pos2 && bz == 0) {
+                        int status = 0;
+                        MPI_Send(&status, 1, MPI_INT, male, my_id, MPI_COMM_WORLD);
+                    } else if (oz == 1) {
+                        int status = 1;
+                        MPI_Send(&status, 1, MPI_INT, male, my_id, MPI_COMM_WORLD);
+                        int data[2];
+                        data[0] = male;
+                        if (bz == 0) {
+                            data[1] = my_id;
+                            MPI_Send(data, 2, MPI_INT, 0, my_id, MPI_COMM_WORLD);
+
+                            status = 0;
+                        } else {
+                            status = 1;
+                        }
+                        MPI_Send(&status, 1, MPI_INT, currently_assigned, my_id, MPI_COMM_WORLD);
+                        currently_assigned = male;
+                    }
+                }
+            }
+
+
+        } else // male
+        {
+            while (1) {
+                if (currently_assigned == 0 && bz == 0) {
+                    for (int i = 0; i < pairs && oz; i++) {
+                        if (rejected_from[pairs + 1 + order[i]] == 0 && bz == 0) {
+                            if (bz == 1) {
+                                cout << "prob here 3";
+                            } else {
+                                MPI_Send(&my_id, 1, MPI_INT, pairs + 1 + order[i], pairs + 1 + order[i],
+                                         MPI_COMM_WORLD);
+                            }
+                            if (oz) {
+                                int status;
+                                if (bz == 1) {
+                                    cout << "prob here 4";
+                                } else {
+                                    MPI_Recv(&status, 1, MPI_INT, pairs + 1 + order[i], pairs + 1 + order[i],
+                                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                                }
+
+                                if (status == 1 && oz) {
+                                    currently_assigned = pairs + 1 + order[i];
+                                    break;
+                                } else if (bz == 0) {
+                                    rejected_from[pairs + 1 + order[i]] = 1;
+                                }
+                            }
+                        }
+                    }
+                } else if (bz == 0) {
+                    int status;
+                    MPI_Recv(&status, 1, MPI_INT, currently_assigned, currently_assigned, MPI_COMM_WORLD,
+                             MPI_STATUS_IGNORE);
+                    if (bz == 1) {
+                        break;
+                    }
+                    currently_assigned = 0;
+                    rejected_from[currently_assigned] = 1;
+
+                }
+            }
+
+        }
     }
-
-    if (isRootProc(my_id)) {
-        // print final_sorted_array in root proc
-        for (auto elem:final_sorted_array)
-            cout << elem << " ";
-    }
-
     MPI_Finalize();
     return 0;
 }
--- readme.md
+++ readme.md
@@ -7,3 +7,23 @@ eg
 ```shell script
  bash q1.sh 4 13 1 4 7 3 2 8 1 4 7 3 2 8 6
 ```
+
+
+bash q2.sh num_processes(2n+1)
+n(# of men/women)
+n lines containing prefs of men
+n lines containing prefs of women
+
+eg
+```shell script
+bash q2.sh 9 \
+4 \
+3 1 2 0 \
+1 0 2 3 \
+0 1 2 3 \
+0 1 2 3 \
+0 1 2 3 \
+0 1 2 3 \
+0 1 2 3 \
+0 1 2 3 
+```
